353f0eb201864cdc0dead4c57b47fed0ceebeaa5
tejpratap46 <tejpratap46@gmail.com>
2016-05-19 14:53:35 +0530

    Added file adapter

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var S3Adapter = require('parse-server/S3Adapter');
 var path = require('path');
 var SimpleSendGridAdapter = require('parse-server-sendgrid-adapter');
 
@@ -35,18 +36,19 @@ var api = new ParseServer({
   appName: 'AppName',
   publicServerURL: 'http://ec2-52-87-221-80.compute-1.amazonaws.com:1337/parse',
   verifyUserEmails: true,
-  // emailAdapter: {
-  //   module: 'parse-server-simple-mailgun-adapter',
-  //   options: {
-  //       fromAddress: 'mymail@example.com',
-  //       apiKey: 'key-3506239551cfd786e4793942f441cb89',
-  //       domain: 'example.com'
-  //   }
-  // }
   emailAdapter: SimpleSendGridAdapter({
     apiKey: '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',
     fromAddress: 'test@example.com',
-  })
+  }),
+  // filesAdapter: new S3Adapter(
+  //   "KEY",
+  //   "SECRET_KEY",
+  //   {
+  //   	bucket: "my-bucket-name",
+  //   	bucketPrefix: "some-folder/",
+  //   	directAccess: true
+  //   }
+  // )
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
